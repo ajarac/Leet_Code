4a5dad460cc88e65b19474e2d4f93044fdb6cac4
Angel Jara <ajarac92@gmail.com>
2022-04-11 15:13:18 +0400

    Binary search

---
+++ easy/binary-search/binary_search_test.go
@@ -0,0 +1,26 @@
+package binary_search
+
+import (
+	"testing"
+)
+
+type input struct {
+	nums   []int
+	target int
+}
+
+func TestBinarySearch(t *testing.T) {
+	var tests = []struct {
+		input    input
+		expected int
+	}{
+		{input{[]int{-1, 0, 3, 5, 9, 12}, 9}, 4},
+		{input{[]int{-1, 0, 3, 5, 9, 12}, 2}, -1},
+		{input{[]int{-1, 0, 3, 5, 9, 12}, 13}, -1},
+	}
+	for _, test := range tests {
+		if output := search(test.input.nums, test.input.target); output != test.expected {
+			t.Error("Binary Search Test Failed: {} inputted, {} expected, received: {}", test.input, test.expected, output)
+		}
+	}
+}
